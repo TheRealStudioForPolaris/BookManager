46f9a79da532582b54760693de50caa2e80e249c
PolarisCodingStudio <ujkl678@outlook.com>
2025-07-16 21:50:17 +0800

    Wrote GET BOOK INFO  function

--- Core/FileIO/Read/read_book_info.py
+++ Core/FileIO/Read/read_book_info.py
@@ -1,4 +1,5 @@
 import os,sys,shutil
+import re
 class ReadBookInfo(object):
     def __init__(self,todo:str,active_id:int):
         self.todo=todo
@@ -42,10 +43,38 @@ class ReadBookInfo(object):
         return self.__check_book_exist(name,isbn)
 
 
-    def __get_book_info(self,name:str|None=None,isbn:str|None=None)->str:
+    def __get_book_info(self,name:str|None=None,isbn:str|None=None)->str|tuple:
         if self.todo!='g':
             return 'EUT'  #ERROR:UNKNOWN TDO
         self.name=name
         self.isbn=isbn
         if not (self.name and self.isbn):
             return 'EUG'   #ERROR:UNKNOWN GOAL
+        filename_tuple = ('name', 'price', 'writer', 'press', 'publish_time', 'isbn', 'intro')
+        self.__book_location=0
+        return_list=[]
+        if self.name:
+            for i in self.__name_list:
+                if f'{self.name}\n'==i:
+                    break
+                self.__book_location+=1
+            for i in filename_tuple:
+                with open(os.path.join(self.__book_file_path,i),'r',errors='ignore') as file:
+                    file_list=file.readlines()
+                    return_list.append(re.sub('/n','\n',str(file_list[self.__book_location])))
+
+        elif self.isbn:
+            for i in self.__isbn_list:
+                if f'{self.isbn}\n'==i:
+                    break
+                self.__book_location+=1
+            for i in filename_tuple:
+                with open(os.path.join(self.__book_file_path,i),'r',errors='ignore') as file:
+                    file_list=file.readlines()
+                    return_list.append(re.sub('/n','\n',str(file_list[self.__book_location])))
+
+        if len(return_list) != len(file_list):
+            return 'NOK'  #NOT OKAY
+        return 'AOK',return_list  #ALL ACTIVES ARE OKAY
+    def gboi(self,name:str|None=None,isbn:str|None=None)->str|tuple:
+        return self.__get_book_info(name,isbn)
