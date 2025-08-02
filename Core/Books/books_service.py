2017f8504da846061c91ec8c89f5f2a99b31ecff
PolarisCodingStudio <ujkl678@outlook.com>
2025-08-02 17:19:23 +0800

    Update books_service.py

--- Core/Books/books_service.py
+++ Core/Books/books_service.py
@@ -51,3 +51,36 @@ class BooksService(object):
                    icons_path:str|None=None
                    )->str|tuple:
         return self.__new_book(name,price,writer,press,publish_time,isbn,intro,icons_path)
+
+    def __search_book_info(self,name:str|None=None,isbn:str|None=None)->str|tuple:
+        if self.todo[0]!='s':
+            return 'EUT'  #ERROR UNKNOWN TDO
+        self.name=name
+        self.isbn=isbn
+        if not (self.name and self.isbn):
+            return 'EUG'   #ERROR:UNKNOWN GOAL
+        import Core.FileIO.Read.read_book_info
+        if self.todo[1]=='f':
+            check_book_exist=Core.FileIO.Read.read_book_info.ReadBookInfo('c',self.active_id)
+            get_book_info=Core.FileIO.Read.read_book_info.ReadBookInfo('g',self.active_id)
+            if self.name:
+                check_book_exist_return = check_book_exist.cbke(self.name)
+                if check_book_exist_return=='BNC':
+                    return 'BNC'  #BOOK WAS NOT CREATED
+                get_book_info_return=get_book_info.gboi(self.name)
+                if get_book_info_return=='NOK':
+                    return 'NOK'   #NOT OKAY
+            elif self.isbn:
+                check_book_exist_return=check_book_exist.cbke(isbn=self.isbn)
+                if check_book_exist_return=='BNC':
+                    return 'BNC'  #BOOK WAS NOT CREATED
+                get_book_info_return=get_book_info.gboi(isbn=self.isbn)
+                if get_book_info_return=='NOK':
+                    return 'NOK'   #NOT OKAY
+            return 'AOK',get_book_info_return[1]
+        elif self.todo[1] == 's':
+            return 'EUT'  # ERROR UNSUPPORTED TDO
+        else:
+            return 'EUT'  # ERROR UNSUPPORTED TDO
+    def sboi(self,name:str|None=None,isbn:str|None=None)->str|tuple:
+        return self.__search_book_info(name,isbn)
